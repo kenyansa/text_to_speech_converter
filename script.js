4c9de51c5e8ec0af15c58aef8b9fc2d8a1b82062
Kenyansa <kenyansajustine@gmail.com>
2023-07-12 16:12:44 +0300

    add code for pausing or playing

--- script.js
+++ script.js
@@ -9,12 +9,26 @@ speechSynthesis.onvoiceschanged = ()=>{
 
     voices.forEach((voice, i)=>(voiceSelect.options[i] = new Option(voice.name, i) ));
 }
-
+//add change event to the dropdown select
 voiceSelect.addEventListener("change", ()=>{
     utterance.voice = voices[voiceSelect.value];
 })
+
+//button click event to convert text to speech or pause/resume
+let isPaused = false;
 document.querySelector("button").addEventListener("click", ()=>{
-    // Set the text and language
-    utterance.text = document.querySelector("textarea").value;
-    speechSynthesis.speak(utterance);
-})
+    if(!isPaused){
+        //set the text
+        utterance.text = document.querySelector("textarea").value;
+        speechSynthesis.speak(utterance);
+    }else{
+        if(speechSynthesis.paused){
+            speechSynthesis.resume();
+            isPaused = false;
+        }else {
+            speechSynthesis.pause();
+            isPaused = true;
+        }
+    }
+    
+});
