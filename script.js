425003fb2f846e8a202c2e07ada18539f12cf86a
Kenyansa <kenyansajustine@gmail.com>
2023-07-12 16:01:00 +0300

    add voices to be changed

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let utterance = new SpeechSynthesisUtterance(); // create SpeechSynthesisUtterance object
+
+let voices = [];
+let voiceSelect = document.querySelector("select")
+
+speechSynthesis.onvoiceschanged = ()=>{
+    voices = speechSynthesis.getVoices();
+    speechSynthesis.voice = voices[0]; //default choice for the voice (first voice)
+
+    voices.forEach((voice, i)=>(voiceSelect.options[i] = new Option(voice.name, i) ));
+}
+
 document.querySelector("button").addEventListener("click", ()=>{
     // Set the text and language
     utterance.text = document.querySelector("textarea").value;
     utterance.lang = 'en-US';
-    window.speechSynthesis.speak(utterance);
+    speechSynthesis.speak(utterance);
 })
-
-// Add the utterance to the speech synthesis queue
-speechSynthesis.speak(utterance);
